db4844217be9277d2fe1541a974dca17315c2dd9
jonasb <jonas.borges182@gmail.com>
2022-10-21 08:48:24 -0300

    feat: first commit

---
+++ storybook-static/assets/preview.b3efd292.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.b6f8ac81.js";import"./iframe.55abd4a8.js";import"./es.map.constructor.deada75b.js";import"./es.number.to-fixed.2ae6de1a.js";import"./index.8c176a1e.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.b3efd292.js.map
